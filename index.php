da139359f8de0c6f76aa0b2b2f22c0737899e46a
ErivaldoOliveiraSobral <erivaldooliveirasobral@gmail.com>
2018-03-24 15:20:32 -0300

    Inserção de novos dados

--- index.php
+++ index.php
@@ -7,20 +7,43 @@
     
 	include_once('Pessoa.php');
 
-	$p = new Pessoa();
+	$p1 = new Pessoa();
 	$p2 = new Pessoa();
+	$p3 = new Pessoa();
+	$p4 = new Pessoa();
+	$p5 = new Pessoa();
+	$p6 = new Pessoa();
+	$p7 = new Pessoa();
+	$p8 = new Pessoa();
+	$p9 = new Pessoa();
+	$p10 = new Pessoa();
 
-	$p->nome = "Erivaldo";
-	$p->email = "erivaldooliveirasobral@gmail.com";
-	$p->senha = "1234";
+	$p1->nome = "Erivaldo";
+	$p1->email = "erivaldooliveirasobral@gmail.com";
+	$p1->senha = "1234";
 
 	$p2->nome = "Felipe";
 	$p2->email = "felipe_ny1@hotmail.com";
 	$p2->senha = "4321";
 
+	$p3->nome = "Guilherme";
+	$p3->email = "guilherme@gmail.com";
+	$p3->senha = "admimadmin";
+
+	$p4->nome = "Gabrielly";
+	$p4->email = "gabrielly@gmail.com";
+	$p4->senha = "admin123";
+
+	$p5->nome = "Jean";
+	$p5->email = "jean@gmail.com";
+	$p5->senha = "1234admin";
+
 	$arrayPessoa = array();
-	array_push($arrayPessoa, $p);
+	array_push($arrayPessoa, $p1);
 	array_push($arrayPessoa, $p2);
+	array_push($arrayPessoa, $p3);
+	array_push($arrayPessoa, $p4);
+	array_push($arrayPessoa, $p5);
 
 	echo json_encode($arrayPessoa);
 ?>
