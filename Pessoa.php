bd589a3c4437d23371d7f698722e49e68dabafd7
ErivaldoOliveiraSobral <erivaldooliveirasobral@gmail.com>
2018-03-23 21:41:57 -0300

    First Commit

---
+++ Pessoa.php
@@ -0,0 +1,11 @@
+<?php
+	/**
+	* 
+	*/
+	class Pessoa 
+	{
+		public $nome;
+		public $email;
+		public $senha;
+	}
+?>
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+	include_once('Pessoa.php');
+
+	$p = new Pessoa();
+	$p2 = new Pessoa();
+
+	$p->nome = "Erivaldo";
+	$p->email = "erivaldooliveirasobral@gmail.com";
+	$p->senha = "1234";
+
+	$p2->nome = "Felipe";
+	$p2->email = "felipe_ny1@hotmail.com";
+	$p2->senha = "4321";
+
+	$arrayPessoa = array();
+	array_push($arrayPessoa, $p);
+	array_push($arrayPessoa, $p2);
+
+	echo json_encode($arrayPessoa);
+?>
