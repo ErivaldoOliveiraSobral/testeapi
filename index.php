422307e97d28ba794a8e91b6cf36b9dcd0fd6a28
ErivaldoOliveiraSobral <erivaldooliveirasobral@gmail.com>
2018-03-23 22:23:45 -0300

    Acess Control *

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
-	header('Access-Control-Allow-Origin: *');
+	header('Access-Control-Allow-Origin: *'); 
+    header("Access-Control-Allow-Credentials: true");
+    header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+    header('Access-Control-Max-Age: 1000');
+    header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+    
 	include_once('Pessoa.php');
 
 	$p = new Pessoa();
